54fdc746f3d1dd3ecf83ef2a2fa552bba25e7f85
Jack Harrison <jackharrison111@gmail.com>
2020-03-02 14:16:30 +0000

    Week 4 SteepestDescent

---
+++ sheet4_q3.m
@@ -0,0 +1,43 @@
+clear all
+
+function f = Q(x)
+  A = [4 2; 2 3];
+  b = [3 1];
+  c = -1;
+  f = 1/2*x'*A*x + b*x + c;
+endfunction
+
+function g = grad(x)
+  A = [4 2; 2 3];
+  b = [3 1];
+  g = 1/2*A'*x + 1/2*A*x + b';
+endfunction
+
+A = [4 2; 2 3];
+b = [3 1];
+c = -1;
+x_0 = [-1,-1]';
+y = Q(x_0);
+g_k = grad(x_0);
+x_k0 = x_0;
+x_k1 = x_k0 - ((g_k'*g_k)/(g_k'*A*g_k))*g_k;
+
+
+function min = SD(start_guess, tolerance)
+  
+  x_k0 = start_guess;
+  g_k = grad(x_k0);
+  A = [4 2; 2 3];
+  x_k1 = x_k0 - (g_k'*g_k)/(g_k'*A*g_k)*g_k;
+  while abs(x_k1-x_k0) > tolerance
+    x_k0 = x_k1;
+    g_k = grad(x_k0);
+    a_k = (g_k'*g_k)/(g_k'*A*g_k);
+    x_k1 = x_k0 - a_k*g_k;
+  endwhile
+  min = x_k1
+endfunction
+tic
+test = SD(x_k0, 10e-3)
+toc
+
---
+++ steepest_descent.m
@@ -0,0 +1,12 @@
+function min = steepest_descent(@func, @grad, start_guess, tolerance, learning_rate):
+  x_k0 = start_guess;
+  g_k = grad(x_k0);
+  a_k = learning_rate;
+  x_k1 = x_k0 - learning_rate*g_k;
+  while abs(x_k1-x_k0) > tolerance
+    x_k0 = x_k1;
+    g_k = grad(x_k0)
+    x_k1 = x_k0 - a_k*g_k;
+  endwhile
+  min = x_k1
+endfunction
